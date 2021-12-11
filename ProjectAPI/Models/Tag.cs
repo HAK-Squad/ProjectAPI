4cbac9305c533c68c0531cf948c8e05a5c5929bd
Zara <zara.baghdasaryan@yahoo.com>
2021-12-11 12:16:13 +0100

    Added the Tag model

---
+++ ProjectAPI/Models/Tag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAPI.Models
+{
+    public class Tag
+    {
+        [Key]
+        public int TagId { get; set; }
+
+        [MaxLength(20), Required]
+        public string TagNumber { get; set; }
+        public Person Person { get; set; }
+        public int PersonId { get; set; }
+        public Door Door { get; set; }
+        public int DoorId { get; set; }
+
+    }
+}
