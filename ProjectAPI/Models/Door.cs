d2d6331fd6700e3c2f7ccff091c702bc7a45713d
Zara <zara.baghdasaryan@yahoo.com>
2021-12-11 11:29:19 +0100

    Created DoorModel

---
+++ ProjectAPI/Models/Door.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAPI.Models
+{
+    public class Door
+    {
+        public int DoorId { get; set; }
+        public string DoorNumber { get; set; }
+        public string DoorLocation { get; set; }
+    }
+}
