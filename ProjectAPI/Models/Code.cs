6f82c0f2acdb52fcd3aa90da30f942f76183a261
Zara <zara.baghdasaryan@yahoo.com>
2021-12-11 11:44:49 +0100

    Added the Code model

---
+++ ProjectAPI/Models/Code.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAPI.Models
+{
+    public class Code
+    {
+        [Key]
+        public int CodeId { get; set; }
+
+        [MaxLength(20), Required]
+        public string CodeString { get; set; }
+    }
+}
--- ProjectAPI/Models/Door.cs
+++ ProjectAPI/Models/Door.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,17 @@ namespace ProjectAPI.Models
 {
     public class Door
     {
+        [Key] //Creating the Identity 
         public int DoorId { get; set; }
+
+        [MaxLength(30), Required] //Setting the limit and that it can't be Null (NotNull)
         public string DoorNumber { get; set; }
+
+        [MaxLength(30), Required]
         public string DoorLocation { get; set; }
+
+        public Code Code { get; set; } //Creating the association
+
+        public int CodeId { get; set; } //Instering the foreign key 
     }
 }
