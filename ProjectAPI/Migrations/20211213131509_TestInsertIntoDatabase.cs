7730457aabd6e3135126b2fd80610fbfb6585926
ZeynabZuwa <zayzuwa@gmail.com>
2021-12-13 14:24:20 +0100

    testar database

---
+++ ProjectAPI/Migrations/20211213131509_TestInsertIntoDatabase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectAPI.Migrations
+{
+    public partial class TestInsertIntoDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            
+
+            INSERT INTO [dbo].[People] ([FirstName],[LastName],[ApertmentNumber]  )
+                VALUES
+                    ('Liam', 'Jönsson', '0101');
+                    
+           
+            ");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM People WHERE FirstName = 'Liam'");
+
+        }
+    }
+}
