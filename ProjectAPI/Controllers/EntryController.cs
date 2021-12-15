faa6875d7a00b0a0a10a0104417f38fa6644a42a
PayamS <72343921+Payam-Shaker@users.noreply.github.com>
2021-12-15 10:56:24 +0100

    Find entries byDoor & byEvent.

---
+++ ProjectAPI/Controllers/EntryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectAPI.Models;
+
+namespace ProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntryController : ControllerBase
+    {
+        private readonly APIprojectDBcontext _context;
+        public EntryController(APIprojectDBcontext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public List<LogEntry> FindEntriesByDoor(string doorName)
+        {
+            var result = _context.LogEntries.Where(le => le.DoorName == doorName).ToList();
+            return result;
+
+        }
+        [HttpGet ("byEvent")]
+        public List<LogEntry> FindEntriesByEvent(string codeString)
+        {
+            var result = _context.LogEntries.Where(cs => cs.CodeString == codeString).ToList();
+            return result;
+
+        }
+
+
+    }
+}
