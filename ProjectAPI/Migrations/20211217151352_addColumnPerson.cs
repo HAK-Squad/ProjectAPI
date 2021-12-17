6968314da56b89540171df87471963079a37de26
Daniel <[daniel23xu@hotmail.com]>
2021-12-17 17:25:10 +0100

    fixed add tags to person and static data

---
+++ ProjectAPI/Migrations/20211217151352_addColumnPerson.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectAPI.Migrations
+{
+    public partial class addColumnPerson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TagNumber",
+                table: "People",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TagNumber",
+                table: "People");
+        }
+    }
+}
